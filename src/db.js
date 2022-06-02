2f5f82239e5bbc41840b67099df20a4bbdb07f42
martin <caceresmartin22@gmail.com>
2022-06-01 23:49:44 -0300

    Heroku_initial
    first_deploy

--- src/db.js
+++ src/db.js
@@ -2,14 +2,10 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
